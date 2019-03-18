1f1f573f50c61756bfebfd4f59764b267383d966
Andrew Csoka <andrew.csoka@iop.org>
2019-03-18 18:57:11 +0000

    fix usage of the env vars

--- main.go
+++ main.go
@@ -31,12 +31,14 @@ func GetStockPrice(w http.ResponseWriter, r *http.Request) {
 	//url := "https://www.alphavantage.co/query?apikey=C227WD9W3LUVKVV9&function=TIME_SERIES_DAILY_ADJUSTED&symbol=MSFT"
 	fmt.Println("SYMBOL:", os.Getenv("SYMBOL"))
 	fmt.Println("NDAYS:", os.Getenv("NDAYS"))
+	fmt.Println("APIKEY:", os.Getenv("APIKEY"))
 
+	apiKey := os.Getenv("APIKEY")
 	symbol := os.Getenv("SYMBOL")
-	ndays := os.Getenv("NDAYS")
+	//ndays := os.Getenv("NDAYS")
 
 	url := "https://www.alphavantage.co/query?apikey=%s&function=TIME_SERIES_DAILY_ADJUSTED&symbol=%s"
-	resp, _ := http.Get(fmt.Sprintf(url, symbol, ndays))
+	resp, _ := http.Get(fmt.Sprintf(url, apiKey, symbol))
 	bytes, _ := ioutil.ReadAll(resp.Body)
 
 	//fmt.Println("HTML:\n\n", string(bytes))
